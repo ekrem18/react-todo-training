f4b28091d3aa786d217cad0459e944d94813ab0a
Ekrem <avekremyilmazturk@gmail.com>
2023-07-26 16:20:04 +0300

    todo comp

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Todo = () => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div classname="todo">
+      <button classname="complete-btn">
+        <i classname="fas fa-check-circle" />
+      </button>
+      <li classname="todo-item" />
+      <button classname="trash-btn">
+        <i classname="fa fa-minus-circle" />
+      </button>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
